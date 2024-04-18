55ca4126674ba4cd725a526bb99bf1289c681f43
PaulRusz <paulruszkay@gmail.com>
2024-04-18 13:20:26 -0400

    models complete

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,32 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+// Product belongs to Category, as a category can have multiple products but a product can only belong to one category.
+Product.belongsTo(Category);
 
 // Categories have many Products
+// Category has many Product models.
+
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
+// Product belongs to many Tag models. Using the ProductTag through model, allow products to have multiple tags and tags to have many products.
+
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  }
+});
 
 // Tags belongToMany Products (through ProductTag)
+// Tag belongs to many Product models.
+Tag.belongsToMany(ProductTag, {
+  through: {
+    model: Product,
+    unique: true,
+  }
+})
 
 module.exports = {
   Product,
@@ -18,3 +38,5 @@ module.exports = {
   Tag,
   ProductTag,
 };
+
+
